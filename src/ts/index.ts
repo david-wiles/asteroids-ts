a3f424ba28172d2ed9a5f4fc879fa52dd47481d4
David Wiles <davidwiles33@gmail.com>
2020-09-22 23:12:52 -0500

    Remove unnecessary html and css, make index.ts pretty

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,19 +2,18 @@ import Simulation from "./Simulation";
 
 document.addEventListener("DOMContentLoaded", function () {
   let canvas = document.querySelector("#game-canvas");
+
   if (canvas instanceof HTMLCanvasElement) {
-    initializeCanvas(canvas);
+
+    canvas.width = window.screen.width;
+    canvas.height = window.screen.height;
+
     let simulation = new Simulation(canvas);
     simulation.registerListeners();
     simulation.setUp();
     simulation.start();
+
   } else {
     console.error("Could not find canvas element.");
   }
 });
-
-
-function initializeCanvas(canvas: HTMLCanvasElement) {
-  canvas.width = window.screen.width;
-  canvas.height = window.screen.height;
-}
